c52af0c32e9e83038c438e87c88db1c5eef4e0d6
CHIACHE,LEE <stu4355226@gmail.com>
2017-07-24 11:28:44 -0700

    bugs fixed

--- src/app/scroll-point.directive.ts
+++ src/app/scroll-point.directive.ts
@@ -13,16 +13,18 @@ export class ScrollPointDirective {
     private el: ElementRef
   ) { }
 
-
   @HostListener('window:scroll', [])
    onWindowScroll() {
       const windowScroll = this.document.body.scrollTop;
       if (windowScroll > this.appScrollPoint) {
         //add class to the native element
         this.renderer.setElementClass(this.el.nativeElement, 'hidden', false);
+        this.renderer.setElementClass(this.el.nativeElement, 'fadeOutLeft', false);
         this.renderer.setElementClass(this.el.nativeElement, 'fadeInLeft', true);
       } else {
         //remove class from native element
+        this.renderer.setElementClass(this.el.nativeElement, 'fadeOutLeft', true);
+        // this.renderer.setElementClass(this.el.nativeElement, 'hidden', true);
         this.renderer.setElementClass(this.el.nativeElement, 'fadeInLeft', false);
       }
 
